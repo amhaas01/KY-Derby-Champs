f7645c130d113e30835a3b221c41a517e1314c00
Annette Haas <ahaas@sd1.org>
2023-03-29 14:14:44 -0400

    update 5

--- README.md
+++ README.md
@@ -18,5 +18,5 @@ Requirements
 Review details from the 'discover.ipynb' notebook and add code to 'clean.py'
 1. Combine data from data sets 
 2. Remove unneeded columns 
-3. Add NA or (?) to missing data
+3. Replace missing data (NaN) with (-)
 
--- clean.py
+++ clean.py
@@ -1,35 +1,28 @@
 import pandas as pd
 import numpy as np
 
-## clean data in KY
+## clean data in KY-Derby_Data1.csv (from Kaggle)
 #
 # Usage: 
 # $ python clean.py data/KY_Derby_Data1.csv results/clean.csv
 
+# where:
+#  KY_Derby_Data1.csv    = C:\Users\ahaas.SDDOMAIN\Desktop\KY-Derby-Champs\data
+#  clean.csv             = C:\Users\ahaas.SDDOMAIN\Desktop\KY-Derby-Champs\results
+
+# Read in Data files
 KY1_df = pd.read_csv('data/KY_Derby_Data1.csv')
 KY2_df = pd.read_csv('data/KY_Derby_Data2.csv')
 
 # drop unneeded columns
 KY1_df.drop(KY1_df.columns[8], axis =1, inplace=True)
-#print(KY1_df.columns[8])
-#print(KY1_df.head(1))
 
 # replace all True/False with Yes/No
-# Replace multiple values
 KY1_df['triple_crown_winner'] = KY1_df['triple_crown_winner'].replace([True],'Yes')
 KY1_df['triple_crown_winner'] = KY1_df['triple_crown_winner'].replace([False],'No')
-#print(KY1_df)
 
-pd.set_option('display.max_rows', None)
-track_times = KY1_df[['track_condition','time_string']].sort_values(by = ['track_condition', 'time_string'], axis=0, ascending=[True, True], inplace=False)
-track_times= track_times.groupby("track_condition").head(3)
-#print(track_times)
 
-#KY1_df_group = KY1_df.groupby("track_condition")
-#KY1_df.group.get_group('Sloppy')
-#print('track_condition')
-
-#Adding a row for the earnings to prepare to drop the earnings row in the next step
+# Adding a row for the earnings to prepare to drop the earnings row in the next step
 KY2_df.loc[-1] = ['2022', '-', '-', '-']
 KY2_df.index = KY2_df.index + 1  # shifting index
 KY2_df.sort_index(inplace=True)
@@ -42,4 +35,10 @@ pd.set_option('display.max_rows', None)
 
 #Replace NaN with NA
 KY1_df['Earnings'] = KY1_df['Earnings'].fillna("-")
-print(KY1_df)
+
+#If I want to drop years prior to 1930
+#KY1_df = KY1_df[KY1_df.Earnings.notnull()]
+#print(KY1_df)
+
+KY1_df.rename(columns = {'track_condition':'Track Condition', 'year':'Year', 'time_string':'Time'}, inplace = True)
+KY1_df.to_csv('results/clean.csv')
