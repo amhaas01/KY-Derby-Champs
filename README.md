324054679fd9dc05e6b2fdd46d2062c6b2f208dc
Annette Haas <ahaas@sd1.org>
2023-03-30 18:40:03 -0400

    final update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # KY-Derby-Champs
+An analysis of the Kentucky Derby Winners from 1875-2022. This project examines the different track conditions in correlation with the speed times. Additionally, this project takes a look at the trainers' top speed times and how many times they have won over time. The project also explores how many times owners have won throughout the history of the Kentucky Derby. After data analysis, Seaborn scatter plot is utilized to visualize the results.
 
 ## Clean Data
 
@@ -10,13 +11,15 @@ Requirements
 - Numpy
 - Seaborn
 
-## Instructions
-
-
 ## Challenges
 
-Review details from the 'discover.ipynb' notebook and add code to 'clean.py'
-1. Combine data from data sets 
+Review details from the 'discover.ipynb' notebook & Clean data via clean.py
+
+Meeting Requirments using below features:
+1. read in data from a local csv
 2. Remove unneeded columns 
 3. Replace missing data (NaN) with (-)
+4. Combine data from data sets 
+5. Markdown files in Jupyter
+6. Visualize data via Seaborn 
 
--- clean.py
+++ clean.py
@@ -15,17 +15,19 @@ KY1_df = pd.read_csv('data/KY_Derby_Data1.csv')
 KY2_df = pd.read_csv('data/KY_Derby_Data2.csv')
 
 # drop unneeded columns
-KY1_df.drop(KY1_df.columns[8], axis =1, inplace=True)
+KY1_df.drop(KY1_df.columns[7], axis =1, inplace=True)
 
 # replace all True/False with Yes/No
 KY1_df['triple_crown_winner'] = KY1_df['triple_crown_winner'].replace([True],'Yes')
 KY1_df['triple_crown_winner'] = KY1_df['triple_crown_winner'].replace([False],'No')
 
-
 # Adding a row for the earnings to prepare to drop the earnings row in the next step
 KY2_df.loc[-1] = ['2022', '-', '-', '-']
 KY2_df.index = KY2_df.index + 1  # shifting index
 KY2_df.sort_index(inplace=True)
+
+#remove $ sign from Earnings column (to later convert string to int or float)
+KY2_df['Earnings']=KY2_df['Earnings'].str.slice(1,20)
 
 # Removes First Row of KY Derby file (since year 2022 is not on Data file 2)
 KY1_df=KY1_df.drop(KY1_df.index[0])
@@ -38,7 +40,7 @@ KY1_df['Earnings'] = KY1_df['Earnings'].fillna("-")
 
 #If I want to drop years prior to 1930
 #KY1_df = KY1_df[KY1_df.Earnings.notnull()]
-#print(KY1_df)
+KY1_df=KY1_df[KY1_df.distance != 1.5]
 
-KY1_df.rename(columns = {'track_condition':'Track Condition', 'year':'Year', 'time_string':'Time'}, inplace = True)
+KY1_df.rename(columns = {'winner':'Winner', 'jockey':'Jockey', 'owner':'Owner', 'distance':'Distance', 'trainer':'Trainer', 'triple_crown_winner':'Triple Crown Winner', 'track_condition':'Track Condition', 'year':'Year', 'time_sec':'Time (Sec)'}, inplace = True)
 KY1_df.to_csv('results/clean.csv')
